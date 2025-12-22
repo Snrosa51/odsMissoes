ba1fb92cbb3e67861710727eadbf2d4a10fc2c9b
snrosa <190318640+Snrosa51@users.noreply.github.com>
2025-12-22 00:13:49 -0300

    Correção de logsss

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const path = require("path");
 const cors = require("cors");
 
 const app = express();
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
