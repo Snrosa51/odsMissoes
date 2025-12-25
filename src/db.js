b8f75bd997e57f0a91adf29bd77f85e902cd09d4
snrosa <190318640+Snrosa51@users.noreply.github.com>
2025-12-24 22:06:38 -0300

    Correção das variáveis para o db.js

--- src/db.js
+++ src/db.js
@@ -1,11 +1,11 @@
 const mysql = require("mysql2/promise");
 
 const pool = mysql.createPool({
-  host: process.env.MYSQLHOST,
-  user: process.env.MYSQLUSER,
-  password: process.env.MYSQLPASSWORD,
-  database: process.env.MYSQLDATABASE,
-  port: process.env.MYSQLPORT,
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: Number(process.env.DB_PORT),
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
@@ -13,6 +13,5 @@ const pool = mysql.createPool({
 });
 
 module.exports = pool;
-
 
 
